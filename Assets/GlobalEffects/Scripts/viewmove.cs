9a0e6dafa40f3ff828409a501af12ae56507d77a
pillowlow <tmps9930613@gmail.com>
2020-11-13 13:39:23 +0800

    fix unused folders and add tileMap Package resorted all the files

---
+++ Assets/GlobalEffects/Scripts/viewmove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class viewmove : MonoBehaviour
+{
+   private float lengthx, startposx, lengthy, startposy; 
+       public GameObject cam;
+       public float parllaxEffectX;
+       public float parllaxEffectY;
+       
+       void Start()
+       {
+           startposx =transform.position.x;
+           lengthx=GetComponent<SpriteRenderer>().bounds.size.x;
+           startposy =transform.position.y;
+           lengthy=GetComponent<SpriteRenderer>().bounds.size.y;
+       }
+   
+       // Update is called once per frame
+        void FixedUpdate()
+       {
+           
+           float distx = ((cam.transform.position.x-startposx) * parllaxEffectX);
+           float disty = ((cam.transform.position.y-startposy) * parllaxEffectY);
+    
+           transform.position=new Vector3(startposx+distx, y: startposy+disty,transform.position.z);
+
+       }
+}
