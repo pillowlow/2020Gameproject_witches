a1efee5a215efdaa02b6f775b92dc8bb9bcdf717
WGsir <eric970604776@gmail.com>
2020-12-12 21:37:57 +0800

    BossAI_v1

---
+++ Assets/Beta Assets/bossTest/boss/ShowRange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowRange : MonoBehaviour
+{
+    public float range;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        //Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(range, 0, 0));
+    }
+}
