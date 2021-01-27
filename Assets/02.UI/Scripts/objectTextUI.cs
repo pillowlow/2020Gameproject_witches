5a5ec842224b8cb2013cc4ab82cad5b1f2dbef6a
ItisCaleb <happycaleb1212@gmail.com>
2021-01-27 19:41:44 +0800

    add Object Interact
    add OnInteract Script at 03.Enviroment
    add objectTextUI Script at 02.UI
    add objectTextUI Prefab at 02.UI
    add rock.json at 02.UI for testing
    只做了簡單的文字視窗

---
+++ Assets/02.UI/Scripts/objectTextUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using Pathfinding.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class objectTextUI : MonoBehaviour
+{
+    private OnInteract Instance;
+    public GameObject UI;
+    private List<String> Text;
+    private bool active;
+    private int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+        UI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+        {
+            if (index >= Text.Count)
+            {
+                
+                active = false;
+                UI.SetActive(false);
+                index = 0;
+                Instance.active = true;
+                Instance = null;
+            }
+            else
+            {
+                UI.GetComponentInChildren<Text>().text = Text[index];
+                if (Input.GetKeyDown(KeyCode.H))
+                {
+                    index++;
+                }
+            }
+            
+        }
+    }
+
+    public void LoadText(String path,OnInteract instance)
+    {
+        Instance = instance;
+        using (StreamReader r = new StreamReader(Application.dataPath+"/02.UI/StoryScripts/"+path))
+        {
+            string json = r.ReadToEnd();
+            Text = (List<String>)TinyJsonDeserializer.Deserialize(json,typeof(List<String>));
+            index = 0;
+            active = true;
+            UI.SetActive(true);
+        }
+    }
+}
---
+++ Assets/03.Enviroment/Scripts/OnInteract.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using static objectTextUI;
+
+public class OnInteract : MonoBehaviour
+{
+    
+    
+    public enum Actions
+    {
+        Story,Item,Event        
+    }
+    
+    public GameObject TextUI; 
+    public bool active = false;
+    public Actions ObjectAction;
+    public String TextPath;
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            active = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            if (active && Input.GetKeyDown(KeyCode.F) )
+            {
+                active = false;
+                DoAction();
+            }
+        }
+        
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            active = false;
+        }
+    }
+    
+    public void DoAction()
+    {
+        switch (ObjectAction)
+            {
+                case Actions.Story:
+                    TextUI.GetComponent<objectTextUI>().LoadText(TextPath,this);
+                    break;
+                case Actions.Item:
+                    break;
+                case Actions.Event:
+                    break;
+            }
+
+    }
+
+}
