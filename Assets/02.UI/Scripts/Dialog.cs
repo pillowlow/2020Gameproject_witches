8645baab47e17344cdb9bfbb4d175e9b1ad678a8
Chad Liu <tcfshchadnt@gmail.com>
2021-01-31 21:30:54 +0800

    add statusBar and dialog

---
+++ Assets/02.UI/Scripts/Dialog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    private Dialog _instance = null;
+
+    public Dialog Instance => _instance;
+
+    [SerializeField] private Text _textArea = null;
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
