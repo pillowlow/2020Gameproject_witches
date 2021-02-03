e943436e3931c1383b3ba64caf4a02ab7af77c9b
Chad Liu <tcfshchadnt@gmail.com>
2021-02-03 21:35:29 +0800

    add CameraSystem.cs

---
+++ Assets/03.Enviroment/Scripts/CameraSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraSystem : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera = null;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        GameObject instancePlayer = PlayerManager.instance.player;
+        if (_virtualCamera != null)
+        {
+            _virtualCamera.Follow = instancePlayer.transform;
+        }
+    }
+}
