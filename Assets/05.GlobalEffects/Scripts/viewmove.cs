308247c1fa8e25673bf1ed9ba3dfd973cb006266
pillowlow <tmps9930613@gmail.com>
2021-03-24 21:34:58 +0800

    added water

--- Assets/05.GlobalEffects/Scripts/viewmove.cs
+++ Assets/05.GlobalEffects/Scripts/viewmove.cs
@@ -8,13 +8,21 @@ public class viewmove : MonoBehaviour
        public GameObject cam;
        public float parllaxEffectX;
        public float parllaxEffectY;
+       public bool bgornot;
+
+       private bool _iscamNull;
+       //private float startassignpostX;
+      //private float startassignpostY;
        
        void Start()
-       {    
+       {
+           _iscamNull = cam == null;
            if(cam == null)
                return;
            camstartposx = cam.transform.position.x;
            camstartposy = cam.transform.position.y;
+           
+
            startposx = transform.position.x;
            startposy = transform.position.y;
        }
@@ -22,18 +30,24 @@ public class viewmove : MonoBehaviour
        // Update is called once per frame
         void Update()
         { 
-            if(cam == null)
+            if(_iscamNull)
                 return;
-            var temp= ParllaxCount(cam.transform.position.x,cam.transform.position.y);
+            var temp= ParllaxCount(cam.transform.position.x,cam.transform.position.y,bgornot:bgornot);
             transform.position=new Vector3(startposx+temp.Item1, y: startposy+temp.Item2,transform.position.z);
 
        }
 
-        (float,float) ParllaxCount(float camx, float camy)
+        (float,float) ParllaxCount(float camx, float camy, bool bgornot)
         {
             float distx, disty;
-            distx = ((cam.transform.position.x-camstartposx) * parllaxEffectX);
-            disty = ((cam.transform.position.y-camstartposy) * parllaxEffectY);
+            distx = ((cam.transform.position.x-0) * parllaxEffectX);
+            disty = ((cam.transform.position.y-0) * parllaxEffectY);
+            if (bgornot == true)
+            {
+                transform.position = new Vector3(cam.transform.position.x, y: cam.transform.position.y, transform.position.z);
+            }
             return (distx, disty);
+            
         }
+        
 }
