f7f59eb8622e323ba8916028468260f0fbdefee4
pillowlow <tmps9930613@gmail.com>
2021-08-09 00:18:29 +0800

    maincha fvx effect (step effect ,dissolve mat shader)

---
+++ Assets/BetaAssets/forpillow/ShaderLibraryTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AnyPortrait;
+
+public class ShaderLibraryTest : MonoBehaviour
+{
+    public apPortrait portrait;
+    public float fadeValue = 0.5f;
+    void Update ()
+       {
+            if ( Input.GetKey(KeyCode.W) )
+            {
+                 fadeValue -= Time.deltaTime * 0.5f;
+                 portrait.SetMeshCustomFloatAll(fadeValue, "_DissolveValue");
+                Debug.Log("tests");
+            }
+            if ( Input.GetKey(KeyCode.Q) )
+            {
+                 fadeValue += Time.deltaTime * 0.5f;
+                 portrait.SetMeshCustomFloatAll(fadeValue, "_DissolveValue");
+            }
+           
+       }
+}
