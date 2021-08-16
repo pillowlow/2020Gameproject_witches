d2056e9ae9240591fcdbb49f8d6dc6b2522ad91c
y ee <qww940714@gmail.com>
2021-08-17 02:34:01 +0800

    PlayParticle

---
+++ Assets/BetaAssets/yee/PlayParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour
+{
+    [SerializeField] List<ParticleSystem> Particles;
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if(Player.gameObject.layer != 12) { return; }
+        foreach (ParticleSystem Particle in Particles)
+        {
+            Particle.Play();
+        }
+    }
+    void OnTriggerExit2D(Collider2D Player)
+    {
+        if(Player.gameObject.layer != 12) { return; }
+        foreach (ParticleSystem Particle in Particles)
+        {
+            Particle.Stop();
+        }
+    }
+}
