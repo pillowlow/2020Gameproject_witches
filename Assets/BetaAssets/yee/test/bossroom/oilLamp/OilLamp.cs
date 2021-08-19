a671417739a17ce797f8d9fa08079b8e5adebd5d
y ee <qww940714@gmail.com>
2021-08-19 16:58:04 +0800

    oilLampFormal

--- Assets/BetaAssets/yee/test/bossroom/oilLamp/OilLamp.cs
+++ Assets/BetaAssets/yee/test/bossroom/oilLamp/OilLamp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class light_yee : MonoBehaviour
+public class OilLamp : MonoBehaviour
 {
     [SerializeField] GameObject light;
     [SerializeField] int DieTime;
@@ -12,9 +12,7 @@ public class light_yee : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D Player)
     {
-        Debug.Log(8);
         if(Player.gameObject.layer != 12) { return; }
-        Debug.Log(9);
         if(playerMovement == null)
         {
             playerMovement = Player.gameObject.GetComponent<PlayerMovement>();
@@ -22,28 +20,30 @@ public class light_yee : MonoBehaviour
         //first went by
         if(!Spilled) 
         {
-            Debug.Log(10);
             Spilled = true;
+            /*
+            if(playerMovement.rig.velocity.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0)
+            }
+            */
             int direction = playerMovement.rig.velocity.x > 0 ? 1 : 0; //if animation fall left
             transform.rotation = Quaternion.Euler(0, 180 * direction, 0);
             GetComponent<Animator>().SetTrigger("Spill");
             return;
         }
-        Debug.Log(0);
         Combustion = StartCoroutine(Combust());
     }
     void OnTriggerExit2D(Collider2D Player)
     {
         if(Player.gameObject.layer != 12) { return; }
         if(Combustion == null) { return; }
-        Debug.Log(2);
         StopCoroutine(Combustion);
     }
     //stay too long will die
     IEnumerator Combust()
     {
         yield return new WaitForSeconds(DieTime);
-        Debug.Log(1);
         playerMovement.Killed();
         yield break;
     }
